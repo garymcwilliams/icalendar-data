380827a139469ff3b49a3d8796df52dbc23d65f0
Gary McWilliams <gary@mc-williams.co.uk>
2019-01-04 13:06:27 +0000

    #3 docker:mount local dropbox folder as datapath location

--- docker-run.bat
+++ docker-run.bat
@@ -1,3 +1,4 @@
-docker run -p 5000:5000 -e "ICAL_OUTPUT=/app-data" -e "ICAL_TEAM=stcolmans" -e "ICAL_YEAR=2018-19" icalendar
+docker run -p 5000:5000 -e "ICAL_TEAM=stcolmans" -e "ICAL_YEAR=2018-19" -e "ICAL_OUTPUT=/opt/ics-data" -v "C:/Users/gmcwilliams/Dropbox:/opt/ics-data" icalendar
 ::debug, gets a terminal to allow inspecting of the image contents
-::docker run -it -p 5000:5000 -e "ICAL_OUTPUT=/app-data" -e "ICAL_TEAM=stcolmans" -e "ICAL_YEAR=2018-19" icalendar /bin/bash
+::docker run -p 5000:5000 -e "ICAL_TEAM=stcolmans" -e "ICAL_YEAR=2018-19" -e "ICAL_OUTPUT=/opt/ics-data" -v "C:/Users/gmcwilliams/Dropbox:/opt/ics-data" icalendar /bin/bash
+

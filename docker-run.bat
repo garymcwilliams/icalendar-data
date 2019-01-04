83f1342d1aa2e8ba7abd6f5e9a9f7a5eb9fe8dff
Gary McWilliams <gary@mc-williams.co.uk>
2019-01-04 08:02:04 +0000

    #3 docker: added debug line for future if needed

--- docker-run.bat
+++ docker-run.bat
@@ -1,1 +1,3 @@
 docker run -p 5000:5000 -e "ICAL_OUTPUT=/app-data" -e "ICAL_TEAM=stcolmans" -e "ICAL_YEAR=2018-19" icalendar
+::debug, gets a terminal to allow inspecting of the image contents
+::docker run -it -p 5000:5000 -e "ICAL_OUTPUT=/app-data" -e "ICAL_TEAM=stcolmans" -e "ICAL_YEAR=2018-19" icalendar /bin/bash

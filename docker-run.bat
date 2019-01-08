47a2bb8a3070e1720fa9c186f9cf06e878ffc19b
Gary McWilliams <gary@mc-williams.co.uk>
2019-01-08 07:58:11 +0000

    #3 docker: provide team_name as env var

--- docker-run.bat
+++ docker-run.bat
@@ -1,8 +1,18 @@
-docker run -p 5000:5000 -e "ICAL_TEAM=stcolmans" -e "ICAL_YEAR=2018-19" -e "ICAL_OUTPUT=/opt/ics-data" -v "C:/Users/gmcwilliams/Dropbox:/opt/ics-data" icalendar
+:: run for team specified by ICAL_TEAM env var
+
+if "%ICAL_TEAM%"=="" goto no_team
+
+docker run -p 5000:5000 -e "ICAL_TEAM=%ICAL_TEAM%" -e "ICAL_YEAR=2018-19" -e "ICAL_OUTPUT=/opt/ics-data" -v "C:/Users/gmcwilliams/Dropbox:/opt/ics-data" icalendar
 
 ::allow dynamic mount of app-data, HOWEVER we prefer to rebuild the image, with data in place on each edit
 ::-e "ICAL_DATAPATH=/app-data" -v "d:/dev/gitrepos/icalendar-data:/app-data"  
 
 ::debug, gets a terminal to allow inspecting of the image contents
 ::docker run -p 5000:5000 -e "ICAL_TEAM=stcolmans" -e "ICAL_YEAR=2018-19" -e "ICAL_OUTPUT=/opt/ics-data" -v "C:/Users/gmcwilliams/Dropbox:/opt/ics-data" icalendar /bin/bash
+goto end
+
+:no_team
+echo set ICAL_TEAM to the teamn name
+
+:end
 

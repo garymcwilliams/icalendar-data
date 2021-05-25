7c64636477f657d7947edd9e76f54dea6eec7655
Gary McWilliams <Gary.McWilliams@verint.com>
2021-05-25 20:39:56 +0100

    docker-run uses OneDrive for storage

--- docker-run.sh
+++ docker-run.sh
@@ -7,4 +7,8 @@ DEBUG=""
 COMP=${1:-stcolmans}
 # allow dynamic mount of app  data, HOWEVER we prefer rebuilding the image with the data in place on each edit
 #-e "ICAL_DATAPATH=/app-data" -v "$PWD:/app-data"
-docker run -p 5000:5000 -e "ICAL_TEAM=${COMP}" -e "ICAL_YEAR=2018-19" -e "ICAL_OUTPUT=/opt/ics-data" -v "/c/Users/gmcwilliams/Dropbox:/opt/ics-data" icalendar $DEBUG
+docker run -p 5000:5000 \
+    -e "ICAL_TEAM=${COMP}" -e "ICAL_YEAR=2018-19" -e "ICAL_OUTPUT=/opt/ics-data" \
+    -v "//c/Users/gmcwilliams/OneDrive:/app/OneDrive" \
+    -v "//c/Users/gmcwilliams/OneDrive:/app/icalendar-data" \
+    icalendar $DEBUG
